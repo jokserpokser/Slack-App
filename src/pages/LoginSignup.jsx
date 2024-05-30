6e638ad0841017b89b40b688a9f15aaee9e1e75f
Eljan Peralta <eljan233@gmail.com>
2024-05-30 13:49:14 +0800

    Fixed redirection of user to dashboard when already logged in

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import Login from '../components/Authentication/Login';
 import Signup from '../components/Authentication/Signup';
+import { useNavigate } from "react-router";
 
 export default function LoginSignup() {
   const [display, setDisplay] = useState("login");
+  const Navigate = useNavigate();
+
+  useEffect(() => {
+    const status = localStorage.getItem("isLoggedIn");
+    if (status === "true"){
+      Navigate('/dashboard');
+    }
+  });
 
   return (
     <div>
