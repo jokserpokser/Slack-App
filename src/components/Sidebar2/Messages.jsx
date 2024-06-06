ab0a1c45d35363be9d4527803be9fa1c1f6e6b60
Eljan Peralta <eljan233@gmail.com>
2024-06-06 22:42:17 +0800

    Added DMs Feature

--- src/components/Sidebar2/Messages.jsx
+++ src/components/Sidebar2/Messages.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
 import "./Sidebar2.css";
 import UserService from "../../services/UserService";
+import { SearchCategory } from "semantic-ui-react";
 
 export default function Messages() {
    const [searchedUser, setSearchedUser] = useState("");
+   const [selectedUser, setSelectedUser] = useState("");
    const [userList, setUserList] = useState([]);
+   const [messages, setMessages] = useState([]);
    const [userListLoading, setUserListLoading] = useState(true);
 
    const [isFocused, setIsFocused] = useState(false);
 
+   const [receiverId, setReceiverId] = useState(1);
+   const [messageInput, setMessageInput] = useState("");
+
+   const [fetchFlag, setFetchFlag] = useState(true);
+
    const user = JSON.parse(localStorage.getItem("user"));
+
    useEffect(() => {
       async function fetchUsers() {
          try {
@@ -25,6 +34,22 @@ export default function Messages() {
       if (userList.length === 0) {
          fetchUsers();
       }
+
+      async function fetchMessages() {
+         if (!fetchFlag) {
+            try {
+               const msg = await UserService.fetchMessages(user, receiverId);
+               setMessages(msg);
+            } catch (error) {
+               alert("Cannot Fetch Messages: Messages.jsx");
+            } finally {
+               setFetchFlag(true);
+               console.log(messages);
+            }
+         }
+      }
+
+      fetchMessages();
    });
 
    const handleSearchUser = (event) => {
@@ -33,7 +58,22 @@ export default function Messages() {
 
    const handleSelectUser = (event) => {
       const email = event.currentTarget.getAttribute("data-email");
+      const selectedId = event.currentTarget.getAttribute("data-id");
+      setReceiverId(selectedId);
       setSearchedUser(email);
+      setSelectedUser(email);
+      setFetchFlag(false);
+   };
+
+   const handleSendMessage = async (e) => {
+      e.preventDefault();
+      try {
+         await UserService.sendMessage(user, receiverId, messageInput);
+         setMessageInput("");
+         setFetchFlag(false);
+      } catch (error) {
+         alert("Message not sent");
+      }
    };
 
    return (
@@ -79,13 +119,14 @@ export default function Messages() {
                            })
                            .sort((a, b) => a.email.localeCompare(b.email))
                            .map((person, index) => {
-                              const { email } = person;
+                              const { email, id } = person;
                               return (
                                  <button
                                     className="searchUser-item"
                                     key={index}
                                     onClick={handleSelectUser}
                                     data-email={email}
+                                    data-id={id}
                                  >
                                     <p>{email}</p>
                                  </button>
@@ -113,25 +154,73 @@ export default function Messages() {
                      .map((person) => {
                         const { email, id } = person;
                         return (
-                           <p className="userItem" key={id}>
+                           <button
+                              className="userItem"
+                              onClick={handleSelectUser}
+                              data-email={email}
+                              data-id={id}
+                              key={id}
+                           >
                               <i className="fa-solid fa-message"></i>
                               {email}
-                           </p>
+                           </button>
                         );
                      })}
                </div>
             </div>
          </div>
          <div className="chatBox-container">
-            <div className="chatMessages-container">asd</div>
-            <div className="chatInput-container">
-               <div className="chatInput2-container">
-                  <form>
-                     <input type="text" />
-                     <input type="submit" value="Send" />
-                  </form>
-               </div>
-            </div>
+            {selectedUser && (
+               <>
+                  <div className="chatMessages-container">
+                     <h6>{selectedUser}</h6>
+                     {!fetchFlag && <p className="loading">Loading Messages</p>}
+                     {fetchFlag &&
+                        messages &&
+                        messages.map((msgs, index) => {
+                           return (
+                              <div
+                                 key={index}
+                                 className={
+                                    user.uid === msgs.sender.uid
+                                       ? "mapMessages-user-container"
+                                       : "mapMessages-receiver-container"
+                                 }
+                              >
+                                 <span className="currentUser">
+                                    {msgs.sender.uid}
+                                 </span>
+                                 <p>
+                                    <span
+                                       className={
+                                          user.uid === msgs.sender.uid
+                                             ? "messageUser"
+                                             : "messageReceiver"
+                                       }
+                                    >
+                                       {msgs.body}
+                                    </span>
+                                 </p>
+                              </div>
+                           );
+                        })}
+
+                     {!messages && <p>No messages to show</p>}
+                  </div>
+                  <div className="chatInput-container">
+                     <div className="chatInput2-container">
+                        <form onSubmit={handleSendMessage}>
+                           <input
+                              type="text"
+                              onChange={(e) => setMessageInput(e.target.value)}
+                              value={messageInput}
+                           />
+                           <input type="submit" value="Send" />
+                        </form>
+                     </div>
+                  </div>
+               </>
+            )}
          </div>
       </div>
    );
--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -1,44 +1,88 @@
-
 import { API_URL } from "../constants/Constants";
 import axios from "axios";
 
 const UserService = {
-    getUsers: async function (user) {
-        try{
-            const headers = {
-                "access-token": user.accessToken,
-                expiry: user.expiry,
-                client: user.client,
-                uid: user.uid
-            }
-            
-            const response = await axios.get(`${API_URL}/users`, {headers});
-            const users = response.data.data;
-            return users.filter((user) => user.id >= 4980);
-        } catch (error) {
-            if(error.response.data.errors){
-                alert(error.response.data.errors);
-            }
-        }
-    },
+   getUsers: async function (user) {
+      try {
+         const headers = {
+            "access-token": user.accessToken,
+            expiry: user.expiry,
+            client: user.client,
+            uid: user.uid,
+         };
 
-    signUp: async function (info) {
-        if (info.password !== info.password_confirmation){
-            return alert ("Passwords do not match!");
-        }
-        try {
-            const response = await axios.post(`${API_URL}/auth/`, info);
-            const { data } = response;
-            if (data.data) {
-                return alert("Account Creation Successful!");
-            }
-        } catch (error) {
-            if (error.response.data.errors){
-                return alert(error.response.data.errors);
-            }
-        }
-    },
-   
-}
+         const response = await axios.get(`${API_URL}/users`, { headers });
+         const users = response.data.data;
+         return users.filter((user) => user.id >= 4980);
+      } catch (error) {
+         if (error.response.data.errors) {
+            alert(error.response.data.errors);
+         }
+      }
+   },
+
+   signUp: async function (info) {
+      if (info.password !== info.password_confirmation) {
+         return alert("Passwords do not match!");
+      }
+      try {
+         const response = await axios.post(`${API_URL}/auth/`, info);
+         const { data } = response;
+         if (data.data) {
+            return alert("Account Creation Successful!");
+         }
+      } catch (error) {
+         if (error.response.data.errors) {
+            return alert(error.response.data.errors);
+         }
+      }
+   },
+
+   fetchMessages: async function (user, receiverId) {
+      const headers = {
+         "access-token": user.accessToken,
+         expiry: user.expiry,
+         client: user.client,
+         uid: user.uid,
+      };
+
+      try {
+         const response = await axios.get(
+            `${API_URL}/messages?receiver_id=${receiverId}&receiver_class=User`,
+            { headers }
+         );
+
+         return response.data.data;
+      } catch (error) {
+         alert(error.response.data.errors);
+      }
+   },
+
+   sendMessage: async function (user, id, messageInput) {
+      const headers = {
+         "access-token": user.accessToken,
+         expiry: user.expiry,
+         client: user.client,
+         uid: user.uid,
+      };
+
+      const body = {
+         receiver_id: id,
+         receiver_class: "User",
+         body: messageInput,
+      };
+
+      try {
+         const response = await axios.post(`${API_URL}/messages`, body, {headers});
+         
+         if (response.data.data) {
+            console.log("Message Sent: ");
+            return response.data;
+         }
+      } catch (error) {
+         console.log("Error: Message not Sent from UserService");
+      }
+   },
+};
 
 export default UserService;
